289d0205cd6737c5142a66e0a78d3684494f0226
Shaik Sajid <shaiksajid492@gmail.com>
2022-06-04 01:30:41 +0530

    1st

---
+++ src/main/java/com/spring/SpringBootUtilitiesOpenSourceApplication.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUtilitiesOpenSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUtilitiesOpenSourceApplication.class, args);
+	}
+
+}
