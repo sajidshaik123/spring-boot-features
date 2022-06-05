bbbd226f32fe88465d8702cbf8a86fdd44b8c1f8
Shaik Sajid <shaiksajid492@gmail.com>
2022-06-05 21:58:06 +0530

    commit

--- src/test/java/com/features/SpringBootFeaturesApplicationTests.java
+++ src/test/java/com/features/SpringBootFeaturesApplicationTests.java
@@ -1,5 +1,6 @@
+package com.features;
 /*
- * package com.spring;
+ * package com.features;
  * 
  * import org.junit.jupiter.api.Test; import
  * org.springframework.boot.test.context.SpringBootTest;
