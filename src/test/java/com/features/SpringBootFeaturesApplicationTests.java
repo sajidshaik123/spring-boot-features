5f2445399f2d07126d668fcece11a9fe35781576
Shaik Sajid <shaiksajid492@gmail.com>
2022-06-05 21:59:13 +0530

    commit

--- src/test/java/com/features/SpringBootFeaturesApplicationTests.java
+++ src/test/java/com/features/SpringBootFeaturesApplicationTests.java
@@ -1,13 +1,13 @@
 package com.features;
-/*
- * package com.features;
- * 
- * import org.junit.jupiter.api.Test; import
- * org.springframework.boot.test.context.SpringBootTest;
- * 
- * @SpringBootTest class SpringBootFeaturesApplicationTests {
- * 
- * @Test void contextLoads() { }
- * 
- * }
- */
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
